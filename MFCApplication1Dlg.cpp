c007a53673a54294ed4b1f30e8b796b7f29cccda
MziKay3 <127722484+MziKay3@users.noreply.github.com>
2025-03-15 15:21:23 +0200

    Add files via upload
    rezultatul este afisat in consola dupa pop-up , trebuie dat scroll in sus un pic.
    Am incarcat doar fisierul modificat, am folosit MFC de tip dialog.

---
+++ MFCApplication1Dlg.cpp
@@ -0,0 +1,164 @@
+﻿//////////////////////////////////////////////////////REZULTATUL APARE IN CONSOLA , TREBUIE DAR SCROLL UN PIC IN SUS
+// MFCApplication1Dlg.cpp : implementation file
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "MFCApplication1.h"
+#include "MFCApplication1Dlg.h"
+#include "afxdialogex.h"
+#include <windows.h>
+#include <stdio.h>
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CAboutDlg dialog used for App About
+void PrintImagePathForSubkeys(HKEY hKey) {
+    DWORD index = 0;
+    DWORD subkeyLength = MAX_PATH;
+    char subkeyName[MAX_PATH];
+
+    while (RegEnumKeyExA(hKey, index++, subkeyName, &subkeyLength, NULL, NULL, NULL, NULL) == ERROR_SUCCESS) {
+        HKEY subkey;
+        if (RegOpenKeyExA(hKey, subkeyName, 0, KEY_READ, &subkey) == ERROR_SUCCESS) {
+            DWORD valueSize = MAX_PATH;
+            char imagePath[MAX_PATH];
+
+            if (RegQueryValueExA(subkey, "ImagePath", NULL, NULL, (LPBYTE)imagePath, &valueSize) == ERROR_SUCCESS) {
+                
+                char outputMessage[1024];
+                snprintf(outputMessage, sizeof(outputMessage), "Subkey: %s\nImagePath: %s\n\n", subkeyName, imagePath);
+                OutputDebugStringA(outputMessage); 
+            }
+
+            RegCloseKey(subkey);
+        }
+
+        subkeyLength = MAX_PATH;
+    }
+}
+class CAboutDlg : public CDialogEx
+{
+public:
+	CAboutDlg();
+
+// Dialog Data
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_ABOUTBOX };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+
+// Implementation
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+CAboutDlg::CAboutDlg() : CDialogEx(IDD_ABOUTBOX)
+{
+}
+
+void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CMFCApplication1Dlg dialog
+
+
+
+CMFCApplication1Dlg::CMFCApplication1Dlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_MFCAPPLICATION1_DIALOG, pParent)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CMFCApplication1Dlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CMFCApplication1Dlg, CDialogEx)
+	ON_WM_SYSCOMMAND()
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+END_MESSAGE_MAP()
+
+
+// CMFCApplication1Dlg message handlers
+
+BOOL CMFCApplication1Dlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	
+	HKEY hKey;
+	if (RegOpenKeyExA(HKEY_LOCAL_MACHINE, "SYSTEM\\CurrentControlSet\\Services", 0, KEY_READ, &hKey) == ERROR_SUCCESS) {
+		
+		PrintImagePathForSubkeys(hKey);
+		RegCloseKey(hKey);
+	}
+	else {
+		AfxMessageBox(_T("Eroare la deschiderea cheii de registru."));
+	}
+
+	return TRUE; 
+}
+
+void CMFCApplication1Dlg::OnSysCommand(UINT nID, LPARAM lParam)
+{
+	if ((nID & 0xFFF0) == IDM_ABOUTBOX)
+	{
+		CAboutDlg dlgAbout;
+		dlgAbout.DoModal();
+	}
+	else
+	{
+		CDialogEx::OnSysCommand(nID, lParam);
+	}
+}
+
+// If you add a minimize button to your dialog, you will need the code below
+//  to draw the icon.  For MFC applications using the document/view model,
+//  this is automatically done for you by the framework.
+
+void CMFCApplication1Dlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // device context for painting
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// Center icon in client rectangle
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// Draw the icon
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialogEx::OnPaint();
+	}
+}
+
+// The system calls this function to obtain the cursor to display while the user drags
+//  the minimized window.
+HCURSOR CMFCApplication1Dlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
